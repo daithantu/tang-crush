a67169debf6947272fe7183d1d7b4ab1129d5a14
daithantu <106808112+daithantu@users.noreply.github.com>
2022-06-03 17:16:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
     introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Em có đièu muốn nói với nhi
+    Nhớ phải trả lời thật lòng nha:>`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    title: 'Nhi iu em đúng hong😙',
+    desc: 'Thoát ra mà không bấm chọn tức là làm vợ em nha:3',
+    btnYes: 'Đúm.Quá chuẩn 😏👉👈',
+    btnNo: 'Mơ',
+    question:'Trên thế giới hơn 7 tỉ người mà sao nhi lại yêu em <3',
+    btnReply: 'Gửi cho em <3',
+    reply: 'Tại vì em cuti s1tg😏',
+    mess: 'Em biết mà :>. IU hnhy của em nhiều nhiều 😘😘',
+    messDesc: 'Mai này lấy về nhi rửa bát nha😏',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/tthn.2403' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
